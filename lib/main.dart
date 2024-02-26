cbce7cb8294ea13aeb4dd1e79f1f9c6306444e81
RoSteik <rostyslav.postolaki@gmail.com>
2024-02-26 22:01:11 +0200

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,23 +20,35 @@ class RockQuestionWidget extends StatefulWidget {
   const RockQuestionWidget({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _RockQuestionWidgetState createState() => _RockQuestionWidgetState();
+  State<RockQuestionWidget> createState() => _RockQuestionWidgetState();
 }
 
 class _RockQuestionWidgetState extends State<RockQuestionWidget> {
   String _displayText = 'Do you like rock?';
+  bool _showLink = false;
   final TextEditingController _controller = TextEditingController();
 
   void _updateDisplayText() {
     setState(() {
       if (_controller.text.trim().toLowerCase() == 'linkin park') {
         _displayText = 'Oh you have taste in music!';
+        _showLink = false; 
       } else {
-        _displayText = 'Give me normal rock group name';
+        _displayText = 'Give me a normal rock group name';
+        _showLink = true; 
       }
     });
   }
+
+
+  Future<void> _launchURL() async {
+    final Uri url = Uri.parse('https://www.youtube.com/watch?v=dQw4w9WgXcQ'); // Rickroll link
+    if (!await launchUrl(url)) {
+      throw 'Could not launch $url';
+    }
+  }
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -46,22 +59,28 @@ class _RockQuestionWidgetState extends State<RockQuestionWidget> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             Text(_displayText, style: const TextStyle(fontSize: 20)),
             Padding(
               padding: const EdgeInsets.all(16),
               child: TextField(
                 key: const Key('responseField'),
                 controller: _controller,
-                // ignore: prefer_const_constructors
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: 'Your answer',
                 ),
-                onSubmitted: (value) {
-                  _updateDisplayText();
-                },
+                onSubmitted: (_) => _updateDisplayText(), 
               ),
             ),
+            if (_showLink) // Display link if _showLink is true
+              GestureDetector(
+                onTap: _launchURL,
+                child: const Text(
+                  'Discover more here!',
+                  style: TextStyle(fontSize: 18, color: Colors.blue,
+                  decoration: TextDecoration.underline),
+                ),
+              ),
           ],
         ),
       ),
