1d30851264e9578da2d8532d2a5f9409e55596d3
RoSteik <rostyslav.postolaki@gmail.com>
2024-04-10 13:21:55 +0300

    handle that functionality is not supported on other platforms

--- lib/lab2/elements/main/pages_list.dart
+++ lib/lab2/elements/main/pages_list.dart
@@ -1,4 +1,5 @@
 import 'package:flash_light_control_plugin/flash_light_control_plugin.dart';
+import 'package:flutter/foundation.dart' show kIsWeb, TargetPlatform;
 import 'package:flutter/material.dart';
 import 'package:my_project/lab2/elements/responsive_config.dart';
 
@@ -10,15 +11,19 @@ List<Widget> pagesList(BuildContext context) {
         mainAxisSize: MainAxisSize.min,
         children: [
           ElevatedButton(
-            onPressed: FlashLightControlPlugin.turnOn,
-            child: Text('Turn ON Flashlight',
-                style: TextStyle(fontSize: contentFontSize),),
+            onPressed: () => _handleFlashlightButton(context, turnOn: true),
+            child: Text(
+              'Turn ON Flashlight',
+              style: TextStyle(fontSize: contentFontSize),
+            ),
           ),
           const SizedBox(height: 20),
           ElevatedButton(
-            onPressed: FlashLightControlPlugin.turnOff,
-            child: Text('Turn OFF Flashlight',
-                style: TextStyle(fontSize: contentFontSize),),
+            onPressed: () => _handleFlashlightButton(context, turnOn: false),
+            child: Text(
+              'Turn OFF Flashlight',
+              style: TextStyle(fontSize: contentFontSize),
+            ),
           ),
         ],
       ),
@@ -27,4 +32,34 @@ List<Widget> pagesList(BuildContext context) {
       child: Text('Activity', style: TextStyle(fontSize: contentFontSize)),
     ),
   ];
+}
+
+void _handleFlashlightButton(BuildContext context, {required bool turnOn}) {
+  if (kIsWeb || Theme.of(context).platform == TargetPlatform.iOS) {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Feature Not Supported'),
+          content: const Text(
+            'Flashlight functionality is not supported on this platform.',
+          ),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  } else {
+    if (turnOn) {
+      FlashLightControlPlugin.turnOn();
+    } else {
+      FlashLightControlPlugin.turnOff();
+    }
+  }
 }
