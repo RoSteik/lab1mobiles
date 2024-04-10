48ac02c7c6ceeeaee5276b626e41a9166ab7a770
RoSteik <rostyslav.postolaki@gmail.com>
2024-04-10 11:50:40 +0300

    lab7

--- lib/lab2/elements/main/pages_list.dart
+++ lib/lab2/elements/main/pages_list.dart
@@ -1,12 +1,27 @@
+import 'package:flash_light_control_plugin/flash_light_control_plugin.dart';
 import 'package:flutter/material.dart';
-
 import 'package:my_project/lab2/elements/responsive_config.dart';
 
 List<Widget> pagesList(BuildContext context) {
   final contentFontSize = ResponsiveConfig.contentFontSize(context);
   return [
     Center(
-      child: Text('Home', style: TextStyle(fontSize: contentFontSize)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ElevatedButton(
+            onPressed: FlashLightControlPlugin.turnOn,
+            child: Text('Turn ON Flashlight',
+                style: TextStyle(fontSize: contentFontSize),),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: FlashLightControlPlugin.turnOff,
+            child: Text('Turn OFF Flashlight',
+                style: TextStyle(fontSize: contentFontSize),),
+          ),
+        ],
+      ),
     ),
     Center(
       child: Text('Activity', style: TextStyle(fontSize: contentFontSize)),
